da33c7c9db7012e7e4d7713444bbf00f908a4235
Ryan Banis <ryanbanis1@gmail.com>
2021-09-15 00:51:37 +0530

    Updated JS and CSS file

---
+++ structure.js
@@ -0,0 +1,30 @@
+function Name(){
+    let name = document.getElementById("name");
+    name.style.color = "black";
+
+}
+function Age(){
+    let age = document.getElementById("age");
+    age.style.color = "black";
+
+}
+function Email(){
+    let email = document.getElementById("email");
+    email.style.color = "black";
+
+}
+function Phone(){
+    let phone = document.getElementById("phone");
+    let dets = document.getElementById("Details")
+    let hi = document.getElementById("hi")
+    phone.style.color = "black";
+    dets.style.display = "flex";
+    hi.style.display = "flex";
+    
+
+
+
+}
+
+
+
